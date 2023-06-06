d41c97e8c773bec03faa93b0eefce17e217fb24e
JuanArcando <2001005@ucc.edu.ar>
2023-06-05 23:26:02 -0300

    Se agrego la funcion para que las respuestas se graben en cultura general y se empezo con la de deportes

---
+++ proyectocg.js
@@ -0,0 +1,55 @@
+// Definir variable global para almacenar las respuestas
+var respuestas = {};
+
+// Función para registrar la respuesta seleccionada por el usuario
+function registrarRespuesta(pregunta, respuesta) {
+    respuestas[pregunta] = respuesta;
+}
+
+// Función para mostrar el resultado final
+function mostrarResultado() {
+    var contadorRespuestas = 0;
+
+    // Calcular el número de respuestas correctas
+    if (respuestas['pregunta1'] === 'a') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta2'] === 'b') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta3'] === 'a') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta4'] === 'b') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta5'] === 'b') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta6'] === 'a') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta7'] === 'a') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta8'] === 'a') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta9'] === 'b') {
+        contadorRespuestas++;
+    }
+    if (respuestas['pregunta10'] === 'c') {
+        contadorRespuestas++;
+    }
+
+    // Mostrar el resultado final
+    alert("Respuestas correctas: " + contadorRespuestas + " de 10");
+
+    // Mostrar todas las respuestas seleccionadas por el usuario
+    var resultadoRespuestas = "Respuestas seleccionadas:\n";
+    for (var pregunta in respuestas) {
+        resultadoRespuestas += pregunta + ": " + respuestas[pregunta] + "\n";
+    }
+    alert(resultadoRespuestas);
+}
+
